748eaaebd55d10cb9af3a11bbd85fdecf7a65b1c
Evgeniy Moshegov <Evgeniy_Moshegov@mail.ru>
2022-06-28 18:57:22 +0200

    fist fersion of slider

---
+++ src/js/script.js
@@ -0,0 +1,29 @@
+const sliderLine = document.querySelector(".slider-line");
+const numOfSlide = sliderLine.childElementCount;
+const widthOfSlide = document.querySelector(".slider").clientWidth;
+const rightOffset = -widthOfSlide * numOfSlide + widthOfSlide;
+let offset = 0;
+let curentSlide = 1;
+
+document.querySelector(".slider-next").addEventListener("click", () => {
+  ++curentSlide;
+  offset -= widthOfSlide;
+  if (curentSlide > numOfSlide) {
+    offset = 0;
+    curentSlide = 1;
+  }
+  sliderLine.style.left = offset + "px";
+  console.log(curentSlide);
+});
+
+document.querySelector(".slider-prev").addEventListener("click", () => {
+  --curentSlide;
+  offset += widthOfSlide;
+  if (curentSlide < 1) {
+    offset = -1500;
+    curentSlide = numOfSlide;
+  }
+  sliderLine.style.left = offset + "px";
+});
+
+console.log(widthOfSlide);
