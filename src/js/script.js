35fe36d1744c7c5f6bf2703ff5c61a0ab9bdd249
Evgeniy Moshegov <Evgeniy_Moshegov@mail.ru>
2022-10-07 09:36:33 +0200

    configured to send messages

--- src/js/script.js
+++ src/js/script.js
@@ -44,4 +44,59 @@ $(document).ready(function () {
   $(".modal__close").on("click", () => {
     $(".overlay, #consultation, #order, #thanks").fadeOut("slow");
   });
+
+  //validate
+
+  function validateForms(form) {
+    $(form).validate({
+      rules: {
+        name: {
+          required: true,
+          minlength: 2,
+        },
+        phone: "required",
+        email: {
+          required: true,
+          email: true,
+        },
+      },
+      messages: {
+        name: {
+          required: "Пожалуйста, введите своё имя",
+          minlength: jQuery.validator.format(
+            "Имя должно быть не мение {0} символов!"
+          ),
+        },
+        phone: "Пожалуйста введите свой номер телефона",
+        email: {
+          required: "Пожалуйста введите свой почтовый адрес",
+          email: "Неправильно введен почтовый адрес",
+        },
+      },
+    });
+  }
+
+  validateForms("#consultation-form");
+  validateForms("#consultation form");
+  validateForms("#order form");
+
+  //masks forms
+
+  $("input[name=phone]").mask("+ 7(999) 999-99-99");
+
+  $("form").submit(function (e) {
+    e.preventDefault(); //отключить обновление страницы
+    $.ajax({
+      //метод отправки из библиотеки query
+      type: "POST", //тип отправки данных
+      url: "mailer/smart.php",
+      data: $(this).serialize(),
+    }).done(function () {
+      $(this).find("input").val("");
+      $("#consultation, #order").fadeOut();
+      $(".overlay, #thanks").fadeIn();
+      $("form").trigger("reset");
+    });
+    return false;
+  });
 });
